1430850ef926e8aed91d5d24b6efd4e8a9e6c856
murichristopher <murichristopher@outlook.com>
2023-11-22 19:19:28 -0300

    Create nuxt app

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Hello Axolutions</h1>
+  <UAvatar
+    src="https://avatars.githubusercontent.com/u/739984?v=4"
+    alt="Avatar"
+  />
+
+  {{ count }}
+
+  <UButton @click="chama">Clicar</UButton>
+</template>
+
+<script setup lang="ts">
+const count: Ref<number> = ref(10);
+
+const chama = () => {
+  count.value += 5;
+};
+</script>
