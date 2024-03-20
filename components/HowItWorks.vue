e7ff15dd467bb100e4a00d29bc26766d4d62564c
Gustavo Alves <alvesgustavo0809@gmail.com>
2024-03-20 17:29:12 -0300

    arruma linha do tempo

--- components/HowItWorks.vue
+++ components/HowItWorks.vue
@@ -1,32 +1,26 @@
 <template>
   <section id="intro" class="mt-20 mx-auto max-w-7xl py-16 md:py-24 lg:px-8">
-    <div class="text-center">
-      <div
-        class="text-4xl/[1.07] font-bold tracking-tight md:text-5xl/[1.07] mb-20"
-      >
-        <span
-          class="bg-gradient-to-br from-white to-zinc-500 bg-clip-text text-transparent"
-          >Descubra
-          <span
-            class="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-purple-900"
-          >
-            como trabalhamos
-          </span>
-        </span>
-      </div>
-    </div>
     <div class="flex justify-center">
-      <div class="container">
-        <div class="timeline">
-          <ul>
+      <div class="container flex items-center justify-center">
+        <div class="timeline relative">
+          <ul class="text-white">
             <li
-              class="rounded-md bg-white-950 ring-1 ring-white/10 lg:rounded-2xl"
+              class="rounded-lg bg-white-950 ring-1 ring-white/10 lg:rounded-2x p-5 mb-10"
               :style="{ opacity: 1, backgroundColor: '#111317' }"
             >
               <div class="timeline-content">
-                <h1>1. Entendemos suas necessidades </h1>
+                <h1
+                  class="font-semibold text-2xl leading-7 mb-4 text-center text-purple-600"
+                  style="color: #bd9ad8"
+                >
+                  1. Entendemos suas necessidades
+                </h1>
                 <p>
-                  Nossa abordagem personalizada começa com uma profunda compreensão de suas metas e objetivos para garantir soluções sob medida que impulsionam o seu sucesso e aumentam sua relevância no mercado, melhorando seu ranking em mecanismos de buscas.
+                  Nossa abordagem personalizada começa com uma profunda
+                  compreensão de suas metas e objetivos para garantir soluções
+                  sob medida que impulsionam o seu sucesso e aumentam sua
+                  relevância no mercado, melhorando seu ranking em mecanismos de
+                  buscas.
                 </p>
               </div>
               <div
@@ -42,13 +36,21 @@
               </div>
             </li>
             <li
-              class="rounded-md bg-white-950 ring-1 ring-white/10 lg:rounded-2xl"
+              class="rounded-md bg-white-950 ring-1 ring-white/10 lg:rounded-2xl p-5 mb-10"
               :style="{ opacity: 1, backgroundColor: '#111317' }"
             >
               <div class="timeline-content">
-                <h1>2. Planejamento estratégico </h1>
+                <h1
+                  class="font-semibold text-2xl leading-7 mb-4 text-center text-purple-600"
+                  style="color: #bd9ad8"
+                >
+                  2. Planejamento estratégico
+                </h1>
                 <p>
-                  Colocamos a criatividade em ação para desenvolver um sistema inovador. Nosso planejamento estratégico leva em consideração seus objetivos e desafios, garantindo soluções quue aumentam sua visibilidade e envolvem seu público-alvo.
+                  Colocamos a criatividade em ação para desenvolver um sistema
+                  inovador. Nosso planejamento estratégico leva em consideração
+                  seus objetivos e desafios, garantindo soluções quue aumentam
+                  sua visibilidade e envolvem seu público-alvo.
                 </p>
               </div>
               <div
@@ -64,13 +66,21 @@
               </div>
             </li>
             <li
-              class="rounded-md bg-white-950 ring-1 ring-white/10 lg:rounded-2xl"
+              class="rounded-md bg-white-950 ring-1 ring-white/10 lg:rounded-2xl p-5 mb-10"
               :style="{ opacity: 1, backgroundColor: '#111317' }"
             >
               <div class="timeline-content">
-                <h1>3. Desenvolvimento do software </h1>
+                <h1
+                  class="font-semibold text-2xl leading-7 mb-4 text-center text-purple-600"
+                  style="color: #bd9ad8"
+                >
+                  3. Desenvolvimento do software
+                </h1>
                 <p>
-                  Nossa equipe altamente qualificada executa o desenvolvimento do seu projeto com eficiência e excelência. Utilizamos tecnologias avançadas e as melhores práticas para garantir resultados de alta qualidade que impulsionam o seu negócio.
+                  Nossa equipe altamente qualificada executa o desenvolvimento
+                  do seu projeto com eficiência e excelência. Utilizamos
+                  tecnologias avançadas e as melhores práticas para garantir
+                  resultados de alta qualidade que impulsionam o seu negócio.
                 </p>
               </div>
               <div
@@ -86,13 +96,21 @@
               </div>
             </li>
             <li
-              class="rounded-md bg-white-950 ring-1 ring-white/10 lg:rounded-2xl"
+              class="rounded-md bg-white-950 ring-1 ring-white/10 lg:rounded-2xl p-5"
               :style="{ opacity: 1, backgroundColor: '#111317' }"
             >
               <div class="timeline-content">
-                <h1>4. Aprovação e refinamento</h1>
+                <h1
+                  class="font-semibold text-2xl leading-7 mb-4 text-center text-purple-600"
+                  style="color: #bd9ad8"
+                >
+                  4. Aprovação e refinamento
+                </h1>
                 <p>
-                  Após o desenvolvimento, entregamos soluções prontas para o sucesso. Nossa dedicação à excelência e à satisfação do cliente garante que você receba resultados concretos que superam as expectativas. Seu sucesso é a nossa prioridade.
+                  Após o desenvolvimento, entregamos soluções prontas para o
+                  sucesso. Nossa dedicação à excelência e à satisfação do
+                  cliente garante que você receba resultados concretos que
+                  superam as expectativas. Seu sucesso é a nossa prioridade.
                 </p>
               </div>
               <div
@@ -115,55 +133,20 @@
 </template>
 
 <style scoped>
-.container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.timeline {
-  position: relative;
-}
-
-.timeline ul {
-  list-style: none;
-}
-.timeline ul li {
-  padding: 20px;
-  color: white;
-  border-radius: 10px;
-  margin-bottom: 40px;
-}
-.timeline ul li:last-child {
-  margin-bottom: 0;
-}
-.timeline-content h1 {
-  font-weight: 500;
-  font-size: 25px;
-  line-height: 30px;
-  margin-bottom: 10px;
-   text-align: center;
-    margin-bottom: 1em;
-    color: #bd9ad8;
-    font-weight: bold;
-}
-
-.timeline-content .date {
-  font-size: 12px;
-  font-weight: 300;
-  margin-bottom: 10px;
-  letter-spacing: 2px;
+.timeline-content p {
+  font-size: 16px;
 }
 @media only screen and (min-width: 880px) {
-
-  .timeline-content h1{
+  .timeline-content h1 {
+    font-size: 28px;
     text-align: center;
     margin-bottom: 1em;
     color: #bd9ad8;
     font-weight: bold;
   }
 
- .timeline-content p{
-    text-align: center;
+  .timeline-content p {
+    font-size: 20px;
   }
   .timeline:before {
     content: "";
@@ -176,7 +159,7 @@
     background-color: #8f8f8f;
   }
   .timeline ul li {
-    padding:40px;
+    padding: 40px;
     width: 50%;
     position: relative;
     margin-bottom: 20px;
@@ -209,10 +192,6 @@
   .timeline ul li:nth-child(even)::before {
     transform: translate(-50%, -50%);
     left: -30px;
-  }
-  .timeline-content .date {
-    position: absolute;
-    top: -30px;
   }
 }
 </style>
